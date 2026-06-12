454d054a85c640053b5600f824e174d3a744b174
SyoBoN <syobon@syobon.net>
2026-06-12 20:43:39 +0900

    create create_nar.sh

---
+++ create_nar.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# SPDX-FileCopyrightText: 2026 SyoBoN <syobon@syobon.net>
+#
+# SPDX-License-Identifier: UPL-1.0
+
+dir=$(dirname "${0}")
+
+pushd "${dir}" > /dev/null
+
+# Build
+zig build -Drelease
+
+# Create updates.txt
+pushd 'public' > /dev/null
+echo 'charset,UTF-8' > updates.txt
+for f in $(find . -type f)
+do
+    echo "file,${f#./}\x01$(md5 -q ${f})\x01size=$(wc -c ${f} | cut -wf2)\x01" >> updates.txt
+done
+cp 'updates.txt' 'ghost/master/'
+
+# Create .nar
+zip -r9 '../ghost_in_a_nutshell.nar' .
+
+popd > /dev/null
+popd > /dev/null
